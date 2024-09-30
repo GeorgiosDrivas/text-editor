f0867a4db5d8a8536264cbe0d46d31320eae21b8
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-09-30 18:31:59 +0300

    Package changes

--- src/components/alignTextActions.tsx
+++ src/components/alignTextActions.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
+import React from "react";
 
 interface T {
-    handleJustify: (x: string) => void
+  handleJustify: (x: string) => void;
 }
 
 const AlignAction = ({ handleJustify }: T) => {
-
-    return (
-      <>
-        <button onClick={() => handleJustify("JustifyLeft")}>
-          <i className="bi bi-justify-left"></i>
-        </button>
-        <button onClick={() => handleJustify("JustifyCenter")}>
-          <i className="bi bi-justify"></i>
-        </button>
-        <button onClick={() => handleJustify("JustifyRight")}>
-          <i className="bi bi-justify-right"></i>
-        </button>
-      </>
-    );
-}
+  return (
+    <>
+      <button onClick={() => handleJustify("JustifyLeft")}>
+        <i className="bi bi-justify-left"></i>
+      </button>
+      <button onClick={() => handleJustify("JustifyCenter")}>
+        <i className="bi bi-justify"></i>
+      </button>
+      <button onClick={() => handleJustify("JustifyRight")}>
+        <i className="bi bi-justify-right"></i>
+      </button>
+    </>
+  );
+};
 
 export default AlignAction;

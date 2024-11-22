33b5a27179d61abb1544b3621258be59ffd848a7
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-11-22 20:31:25 +0200

    Align-action buttons active state CSS

--- src/components/alignTextActions.tsx
+++ src/components/alignTextActions.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 
 interface T {
-  handleJustify: (x: string) => void;
+  handleJustify: (e: MouseEvent<HTMLButtonElement>, x: string) => void;
 }
 
 const AlignAction = ({ handleJustify }: T) => {
   return (
     <>
-      <button onClick={() => handleJustify("JustifyLeft")}>
+      <button onClick={(e) => handleJustify(e, "JustifyLeft")}>
         <i className="bi bi-justify-left"></i>
       </button>
-      <button onClick={() => handleJustify("JustifyCenter")}>
+      <button onClick={(e) => handleJustify(e, "JustifyCenter")}>
         <i className="bi bi-justify"></i>
       </button>
-      <button onClick={() => handleJustify("JustifyRight")}>
+      <button onClick={(e) => handleJustify(e, "JustifyRight")}>
         <i className="bi bi-justify-right"></i>
       </button>
     </>
--- src/utils/handleJustify.ts
+++ src/utils/handleJustify.ts
@@ -1,4 +1,10 @@
-export const handleJustify = (justify: string) => {
+import { MouseEvent } from "react";
+
+export const handleJustify = (
+  e: MouseEvent<HTMLButtonElement>,
+  justify: string
+) => {
+  e.currentTarget.classList.toggle("activeButton");
   const selection: any = window.getSelection();
   const selectedText = selection.toString();
   if (selectedText !== "") {
